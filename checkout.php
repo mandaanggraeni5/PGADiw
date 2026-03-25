e89dd481d9f784a821a0b7e8d9c234ac97fc808c
mandaanggraeni5 <mandaanggraeni5@gmail.com>
2026-03-25 11:23:57 +0700

    Add checkout form for order submission

---
+++ checkout.php
@@ -0,0 +1,12 @@
+<form action="order.php" method="POST">
+Nama:
+<input type="text" name="nama" required>
+
+Metode:
+<select name="metode">
+    <option value="qris">QRIS</option>
+    <option value="cod">COD</option>
+</select>
+
+<button>Order</button>
+</form>
